3af90c45f75325f14c549e2f552bc82ee6d68103
eichc <eich.cameron@gmail.com>
2024-07-16 15:04:19 -0400

    changing from wav to mp3 to save $

--- backend/test.py
+++ backend/test.py
@@ -1,6 +1,6 @@
 from transcribeaudio import *
 
-audio_file_path = "./audio/ShortEarningsCall.wav"
+audio_file_path = "./audio/EarningsCall.mp3"
 transcription = transcribe_audio(audio_file_path)
 print(transcription)
 #minutes = meeting_minutes(transcription)
